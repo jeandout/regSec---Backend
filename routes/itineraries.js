cdf41f66e5722ad4624245d49bef881429c53402
jeandout <jeandout@hotmail.fr>
2025-01-24 16:00:49 +0100

    adding itinéraries route to get routes from geojson file

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 /* GET itinéraries */
 router.get('/waypoints', async (req, res) => {
-  const geojsonPath = path.join(__dirname, "../GEOJSON/waypoints.geojson");
+  const geojsonPath = path.join(__dirname, "../GEOJSON/Points.geojson");
 
   fs.readFile(geojsonPath, 'utf8', (err, data) => {
     if (err) {
@@ -34,5 +34,37 @@ router.get('/waypoints', async (req, res) => {
     res.status(200).json(filteredFeatures);
   });
 });
+
+router.get('/routes', async (req, res) => {
+  const geojsonPath = path.join(__dirname, "../GEOJSON/AdultesParcours.geojson");
+
+  fs.readFile(geojsonPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Erreur lors de la lecture du fichier GeoJSON:', err.message);
+      return res.status(500).json({
+        result: false,
+        message: 'Erreur lors de la lecture du fichier GeoJSON',
+        error: err.message,
+      });
+    }
+    const geojsonData = JSON.parse(data);
+
+    // Filtrer les propriétés à renvoyer (par exemple, en supprimant "properties" et en gardant "geometry")
+    const filteredFeatures = geojsonData.features.map(feature => {
+      return {
+        name: feature.properties.name,  
+        coordinates: feature.geometry.coordinates, // Garder la géométrie
+        // Vous pouvez choisir d'inclure uniquement certaines propriétés (par exemple "properties.name")
+        properties: {
+          name:feature.properties.name,
+          description:feature.properties.description
+        }
+      };
+    });
+    // Envoyer le contenu du fichier comme JSON
+    res.status(200).json(filteredFeatures);
+  });
+});
+
 
 module.exports = router;
